545236d387c93b15814f9170c4e26f54a50c8029
Kendrick Taylor <kendrick@circuitsix.com>
2019-02-22 11:55:31 -0800

    fix multiple defer calls, add tests, refactor old tests

--- tests/common.js
+++ tests/common.js
@@ -1,6 +1,13 @@
 
 var _ = require('dry-underscore');
 
+function async_wait(callback, msec){ 
+   setTimeout(function(){
+      return callback(msec);
+   }, msec);
+}
+
+
 function async_echo(callback){ 
    var args = _.a(arguments).slice(1);
    setTimeout(function(){
@@ -14,11 +21,17 @@ function async_inc(callback, i){
    }, (Math.random() * 10));
 }
 
+function immediate(callback, x){ 
+   var args = _.a(arguments).slice(1);
+   return callback.apply(null, args);
+}
+
 var { eq, ok } = _.test;
-var async = { echo: async_echo, inc: async_inc };
+var async = { echo: async_echo, inc: async_inc, wait: async_wait };
 
 module.exports = {
    _,
    async,
+   immediate,
    ok, eq
 };
