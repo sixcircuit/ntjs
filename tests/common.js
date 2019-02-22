84657327dd5e56ecac51dfe338edec18bab3cd5c
Kendrick Taylor <kendrick@circuitsix.com>
2019-02-21 20:35:04 -0800

    imported and combined old tests. nothing works right now.

--- tests/common.js
+++ tests/common.js
@@ -5,12 +5,19 @@ function async_echo(callback){
    var args = _.a(arguments).slice(1);
    setTimeout(function(){
       return callback.apply(null, args);
-   }, Math.random() * 10);
+   }, (Math.random() * 10));
+}
+
+function async_inc(callback, i){ 
+   setTimeout(function(){
+      return callback(i+1);
+   }, (Math.random() * 10));
 }
 
 var { eq, ok } = _.test;
+var async = { echo: async_echo, inc: async_inc };
 
 module.exports = {
-   async_echo,
+   async,
    ok, eq
 };
