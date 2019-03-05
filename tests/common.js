011b71d28ebf17762e2fed283b0af0cb434c5357
Kendrick Taylor <kendrick@circuitsix.com>
2019-03-05 01:53:37 -0800

    fix up some tests, make array assignments work (working on it), fix up error messages

--- tests/common.js
+++ tests/common.js
@@ -1,6 +1,8 @@
 
 var _ = require('dry-underscore');
 
+var { eq, ok } = _.test;
+
 function async_wait(callback, msec){ 
    setTimeout(function(){
       return callback(msec);
@@ -34,12 +36,34 @@ function immediate(callback, x){
    return callback.apply(null, args);
 }
 
-var { eq, ok } = _.test;
+// replace this with _.test.timer
+function timer(abs){
+   let start = _.timestamp();
+   return(function(expected_diff, margin){
+      margin = margin || 0;
+      const diff = _.timestamp() - start;
+      if(diff < expected_diff){
+         let err = _.error("not_eq", `timer expected: ${diff} < ${expected_diff}`);
+         err.actual = diff;
+         err.expected = expected_diff;
+         throw err;
+      }else if(diff > (expected_diff + margin)){
+         let err = _.error("not_eq", `timer expected: ${diff} > ${ expected_diff + margin } (${expected_diff} + margin(${margin}))`);
+         err.actual = diff;
+         err.expected = expected_diff;
+         throw err;
+      }
+      if(abs !== true){ start = _.timestamp(); }
+   });
+}
+
+
 var async = { echo: async_echo, inc: async_inc, wait: async_wait };
 
 module.exports = {
    _,
    async,
+   timer,
    immediate,
    ok, eq
 };
